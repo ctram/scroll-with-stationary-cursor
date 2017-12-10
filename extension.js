8487663315728841f05ff450e96eaba3373abd20
Chris Tram <chris.tram@gmail.com>
2017-12-10 11:30:05 -0800

    refactor registration of commands.

--- extension.js
+++ extension.js
@@ -1,28 +1,17 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 function activate(context) {
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable1 = vscode.commands.registerCommand('extension.scrollWithCursorUp', function () {
-    vscode.commands.executeCommand("cursorUp");
+    const scrollWithCursorUp = vscode.commands.registerCommand('extension.scrollWithCursorUp', function () {
+        vscode.commands.executeCommand("cursorUp");
         vscode.commands.executeCommand("scrollLineUp");
     });
-    let disposable2 = vscode.commands.registerCommand('extension.scrollWithCursorDown', function () {
-    vscode.commands.executeCommand("cursorDown");
+    const scrollWithCursoDown = vscode.commands.registerCommand('extension.scrollWithCursorDown', function () {
+        vscode.commands.executeCommand("cursorDown");
         vscode.commands.executeCommand("scrollLineDown");
     });
-
-    context.subscriptions.push(disposable1);
-    context.subscriptions.push(disposable2);
+    context.subscriptions = [scrollWithCursorUp, scrollWithCursoDown];
 }
+
 exports.activate = activate;
-
-// this method is called when your extension is deactivated
-function deactivate() {
-}
+function deactivate() {}
 exports.deactivate = deactivate;
