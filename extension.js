7bd3bc535509d04dedaeafcb770dd31aee6aad5a
Chris Tram <chris.tram@gmail.com>
2017-12-10 11:32:06 -0800

    fox typo

--- extension.js
+++ extension.js
@@ -1,15 +1,15 @@
 const vscode = require('vscode');
 
 function activate(context) {
-    const scrollWithCursorUp = vscode.commands.registerCommand('extension.scrollWithCursorUp', function () {
+    const disposableScrollWithCursorUp = vscode.commands.registerCommand('extension.scrollWithCursorUp', function () {
         vscode.commands.executeCommand("cursorUp");
         vscode.commands.executeCommand("scrollLineUp");
     });
-    const scrollWithCursoDown = vscode.commands.registerCommand('extension.scrollWithCursorDown', function () {
+    const disposableScrollWithCursorDown = vscode.commands.registerCommand('extension.scrollWithCursorDown', function () {
         vscode.commands.executeCommand("cursorDown");
         vscode.commands.executeCommand("scrollLineDown");
     });
-    context.subscriptions = [scrollWithCursorUp, scrollWithCursoDown];
+    context.subscriptions = [disposableScrollWithCursorUp, disposableScrollWithCursorDown];
 }
 
 exports.activate = activate;
